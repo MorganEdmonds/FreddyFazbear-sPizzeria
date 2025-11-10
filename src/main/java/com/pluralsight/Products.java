361f8824ea32de8d0ecc759594a0932b22989ecf
MorganEdmonds <morganedmonds@gmail.com>
2025-11-10 09:52:27 -0500

    created product class and added some sizes

--- src/main/java/com/pluralsight/Products.java
+++ src/main/java/com/pluralsight/Products.java
@@ -1,6 +1,26 @@
 package com.pluralsight;
 
 public abstract class Products {
+    protected String size = "S, M, L";
+    protected double price;
 
+    public Products(String size, double price) {
+        this.size = size;
+        this.price = price;
+    }
 
+    public String getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract double calculatePrice();
+
+    @Override
+    public String toString() {
+        return String.format("%s | $%.2f", size, price);
+    }
 }
